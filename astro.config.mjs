1d5020c0590df48f032dc9556651b7e3cb9f80cf
kohei <kouhei.stifler@gmail.com>
2025-07-29 15:32:31 +0900

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dddynamis.com',  // ← 必須。実際のドメインにすること！
+  output: "server", // SSRを有効に
+  adapter: node({ mode: "standalone" }),  // Node.jsで実行する場合は adapter 不要
+  site: "https://dddynamis.com", // ← 必須。実際のドメインにすること！
   integrations: [react(), sitemap()],
 });
